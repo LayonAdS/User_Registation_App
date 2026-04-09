0e3461b6de7449fc45e84db4ccd566b5266e388d
LayonAdS <locosilia5@gmail.com>
2026-04-09 20:56:52 -0300

    conexão com o banco de dados

--- app/src/main/java/com/example/userregistrationapp/ReportActivity.java
+++ app/src/main/java/com/example/userregistrationapp/ReportActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import  androidx.room.Room;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class ReportActivity extends AppCompatActivity {
     // campo onde os dados do banco serao mostrados
@@ -21,6 +24,26 @@ public class ReportActivity extends AppCompatActivity {
         Button btnVoltar = findViewById(R.id.btnVoltar);
         //o botão de retorno utilizando expressão lamdda
         btnVoltar.setOnClickListener(v -> voltarParaCadastro());
+        /* conexao com o bando de dados
+          1- cria uma instancia do banco 'user-database'
+          2- allowMainThreadQuieres():Serve para liberar operações de consultas feitas em thread da UI
+          Por padrão, ROMM proibe isoo. o correto seria fazer consultas em threads seperadas
+         */
+        UserDatabase db = Room.databaseBuilder(getApplicationContext(),UserDatabase.class, "user-database").allowMainThreadQueris().build();
+        // obtem o objeto dao(data access object) que contem as queries SQL
+        UserDao userDao = db.userDao();
+        // Recupera todos os dados salvos no bd e armazena numa lista
+        List<User> userList = userDao.getALLUsers();
+        //sTRINGbUILDER FORMA EFICIENTE DE CONTRUIR UMA sTRING LONGA DENTRO DE U7M LAÇO DE REPETIÇÃO
+        StringBuilder report = new StringBuilder();
+
+        // loop "for-wach" para percorrer cada objeto User dentro da lista reparada
+        for(User user: userList)
+        {
+            report.append("Nome> ").append(user.getName()).append("\n").append("CPF: ").append(user.getCPF()).append("\n\n");
+        }
+        // Exibe o relatorio final montado na TextView da tela
+        textViewReport.setText(report.toString());
     }
 
     //Metodo responsavel pela navegação entre as telas de app
